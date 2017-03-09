e97c22660314dd7a6c6831f765f8afbb2c9d4b19
lerkasan <lerkasan@gmail.com>
2017-03-09 03:04:31 +0200

    fixed sql inserting in user table in test profile

--- src/main/resources/sql/data-test-mysql-000.sql
+++ src/main/resources/sql/data-test-mysql-000.sql
@@ -3,15 +3,15 @@ INSERT INTO role (title) VALUES ('ROLE_ADMIN');
 INSERT INTO role (title) VALUES ('ROLE_EXPERT');
 INSERT INTO role (title) VALUES ('ROLE_MODERATOR');
 
-INSERT INTO user (username, email, password_hash, creation_date) VALUES
+INSERT INTO user (username, email, password_hash, creation_timestamp, enabled) VALUES
   ('some_user1', 'some_user1@mail.com', '$2a$10$cHVXytfvSSDn5TAm5T/SfedKlSbEzBvFt2Y.AOqgA5jfhIOAlgZ4m',
-   now());
-INSERT INTO user (username, email, password_hash, creation_date) VALUES
+   now(), true);
+INSERT INTO user (username, email, password_hash, creation_timestamp, enabled) VALUES
   ('admin', 'some_admin1@mail.com', '$2a$10$V/MgrWzBqwypVcu/RQMPt.iBYQlpHVHycyaGXCM.UwyeOLoSg6VwO',
-   now());
-INSERT INTO user (username, email, password_hash, creation_date) VALUES
+   now(), true);
+INSERT INTO user (username, email, password_hash, creation_timestamp, enabled) VALUES
   ('expert', 'some_expert1@mail.com', '$2a$10$U2JdYU2ypsJVS3SFZD8mue68PZLWXSySjrYKtVL44.uZl3KflfXRa',
-   now());
+   now(), true);
 
 INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO user_role (user_id, role_id) VALUES (2, 2);
